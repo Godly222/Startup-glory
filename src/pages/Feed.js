373ddfff80d1de307a16b42297c0634ac78ba985
Godly222 <godlyid@proton.me>
2026-05-07 12:48:06 +0100

    Create Feed.js

---
+++ src/pages/Feed.js
@@ -0,0 +1,13 @@
+function Feed() {
+  return (
+    <div className="container">
+      <h1>Community Feed</h1>
+
+      <div className="card">
+        Founder updates will appear here.
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
