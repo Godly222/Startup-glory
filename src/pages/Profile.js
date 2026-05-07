27a9b7b34191b69b830d46f1ee6b0076b9ec59df
Godly222 <godlyid@proton.me>
2026-05-07 12:49:43 +0100

    Create Profile.js

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+function Profile() {
+  return (
+    <div className="container">
+      <h1>Founder Profile</h1>
+
+      <div className="card">
+        Certificates and achievements appear here.
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
