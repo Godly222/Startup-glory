741fa2afe7c701739740d1343e56d582696b7713
Godly222 <godlyid@proton.me>
2026-05-07 12:40:43 +0100

    Create Home.js

---
+++ public/src/pages/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="container">
+      <h1>Build. Prove. Grow.</h1>
+
+      <p>
+        Startup Glory supports entrepreneurs across Africa,
+        the UK and globally.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
