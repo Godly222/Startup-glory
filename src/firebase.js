d79f2635b05fd97a499b46405e9c8eeb1de8f75e
Godly222 <godlyid@proton.me>
2026-05-07 15:57:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyB3fMh7jmsuazqWp9naUxlp-uyZK1HFUDk",
   authDomain: "startupglory.firebaseapp.com",
   projectId: "startupglory",
+  storageBucket: "startupglory.firebasestorage.app",
+  messagingSenderId: "755518409025",
+  appId: "1:755518409025:web:f5f55233e331172d01b9c0",
+  measurementId: "G-MH91QXFJL0"
 };
 
 const app = initializeApp(firebaseConfig);
