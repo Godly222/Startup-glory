d9928cb522246db326e984873105d87d46b6d876
Godly222 <godlyid@proton.me>
2026-05-07 15:56:29 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+  apiKey: "AIzaSyB3fMh7jmsuazqWp9naUxlp-uyZK1HFUDk",
+  authDomain: "startupglory.firebaseapp.com",
+  projectId: "startupglory",
 };
 
 const app = initializeApp(firebaseConfig);
