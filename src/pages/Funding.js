187328eca89ee9df9931d1ba2df7feb08e1c135e
Godly222 <godlyid@proton.me>
2026-05-07 12:48:55 +0100

    Create Funding.js

---
+++ src/pages/Funding.js
@@ -0,0 +1,17 @@
+function Funding() {
+  return (
+    <div className="container">
+      <h1>Support Startups</h1>
+
+      <div className="card">
+        Reward-based support only.
+      </div>
+
+      <p>
+        No investment returns or equity offered.
+      </p>
+    </div>
+  );
+}
+
+export default Funding;
